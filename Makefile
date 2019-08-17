7487a4acd85e9292ba10e7b5da7d6d02b41af024
illuminati1911 <ville.pv@gmail.com>
2019-08-17 09:00:08 +0000

    Removed pigpio, irslinger and replaced it with IRBlaster kernel module

--- Makefile
+++ Makefile
@@ -1,29 +1,9 @@
 .DEFAULT_GOAL := all
-
-get-pigpio:
-	@echo "Fetching and installing pigpio..."
-	@git clone https://github.com/joan2937/pigpio.git
-	@cd pigpio \
-	&& make \
-	&& sudo make install
-	@sudo rm -rf pigpio
-	@echo "Installing pigpio completed!"
-update-irslinger:
-	@echo "Fetching ir-slinger..."
-	@git clone https://github.com/bschwind/ir-slinger.git
-	@rm -f internal/accontrol/hwinterface/irslinger.h
-	@mv ir-slinger/irslinger.h internal/accontrol/hwinterface/
-	@rm -rf ir-slinger
-deps: get-pigpio
-	@echo ""
-	@echo ""
-	@echo "===================================="
-	@echo "Dependencies fetched and installed!"
+all: build test
 build:
 	@echo "Building Goira..."
-	@go build ./cmd/main.go
+	@go build -o goira ./cmd/main.go
 	@echo "Build completed!"
-all: deps build test
 test:
 	@go test -race ./...
 	@go vet ./...
--- internal/accontrol/hwinterface/gpio_interface.go
+++ internal/accontrol/hwinterface/gpio_interface.go
@@ -1,15 +1,13 @@
 package hwinterface
 
-// #cgo CFLAGS: -g -Wall
-// #cgo LDFLAGS: -lm -lpigpio -pthread -lrt
-// #include <stdlib.h>
-// #include "ir_interface.h"
-import "C"
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
+	"os"
+
 	"github.com/illuminati1911/goira/internal/accontrol"
 	"github.com/illuminati1911/goira/internal/models"
-	"unsafe"
 )
 
 // GPIOInterface handles mapping the ACState model to binary code
@@ -18,6 +16,25 @@ import (
 type GPIOInterface struct {
 	mapper accontrol.Mapper
 	pin    uint
+}
+
+// iRBlasterConfig is buffer data structure that
+// is sent to the kernel driver at /dev/irblaster
+//
+// See also: https://github.com/illuminati1911/IRBlaster
+//
+type iRBlasterConfig struct {
+	LeadingPulseWidth  uint32
+	LeadingGapWidth    uint32
+	OnePulseWidth      uint32
+	OneGapWidth        uint32
+	ZeroPulseWidth     uint32
+	ZeroGapWidth       uint32
+	TrailingPulseWidth uint32
+	Frequency          uint32
+	DCN                uint32
+	DCM                uint32
+	code               [0x200]byte
 }
 
 // NewGPIOInterface creates new instance of GPIOInterface
@@ -30,12 +47,36 @@ func NewGPIOInterface(mapper accontrol.Mapper, pin uint) accontrol.HWInterface {
 // to the irslinger.
 //
 func (gpio *GPIOInterface) SetState(newState models.ACState) error {
-	pin := C.uint(gpio.pin)
-	data := C.CString(gpio.mapper.MapToProtocolBinaryString(&newState))
-	defer C.free(unsafe.Pointer(data))
-	err := C.runIR(pin, data)
-	if err != 0 {
-		return errors.New("Failure sending IR")
+	// TODO: Enabled when IRBlaster supports other pins than 18:
+	//
+	//pin := C.uint(gpio.pin)
+	var b [0x200]byte
+	data := gpio.mapper.MapToProtocolBinaryString(&newState)
+	copy(b[:], data)
+	cfg := iRBlasterConfig{
+		LeadingPulseWidth:  9000,
+		LeadingGapWidth:    4500,
+		OnePulseWidth:      560,
+		OneGapWidth:        1680,
+		ZeroPulseWidth:     560,
+		ZeroGapWidth:       560,
+		TrailingPulseWidth: 560,
+		Frequency:          38000,
+		DCN:                50,
+		DCM:                100,
+		code:               b,
 	}
+	buf := &bytes.Buffer{}
+	err := binary.Write(buf, binary.LittleEndian, cfg)
+	if err != nil {
+		return errors.New("could not generate IRBlaster transmission buffer")
+	}
+
+	f, err := os.OpenFile("/dev/irblaster", os.O_WRONLY, 0600)
+	if err != nil {
+		return errors.New("could not open kernel driver file descriptor")
+	}
+	defer f.Close()
+	f.Write(buf.Bytes())
 	return nil
 }
